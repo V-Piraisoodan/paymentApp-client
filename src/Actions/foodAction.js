e855fa9623da3d44676768bf7dcedcf6e6d1ab6c
V.Piraisoodan <piraisoodanv1996@gmail.com>
2023-01-22 13:58:35 +0530

    api updated

--- src/Actions/foodAction.js
+++ src/Actions/foodAction.js
@@ -5,7 +5,7 @@ export const getAllFoods = ()=>async dispatch=>{
     dispatch({type : "GET_FOODS_REQUEST"})
 
     try{
-        const response = await axios.get("/api/foods/getfoods")
+        const response = await axios.get("https://payment-server-996t.onrender.com/api/foods/getfoods")
         console.log(response)
         dispatch({type : "GET_FOODS_SUCCESS" , payload : response.data})
     }
